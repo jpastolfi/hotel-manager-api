3f6099405619d96412ae6f69ceff2c2c99a8a59a
Joao Astolfi <jpastolfi@gmail.com>
2024-01-15 23:32:47 +0000

    feat: criação das models Room, Hotel e City. Configuração do context para a entidade TrybeHotel

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,6 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
-public class Hotel {
-
+public class Hotel
+{
+  [Key]
+  public int HotelId { get; set; }
+  public string? Name { get; set; }
+  public string? Address { get; set; }
+  [ForeignKey("CityId")]
+  public int CityId { get; set; }
+  public City? City { get; set; }
+  public ICollection<Room>? Rooms { get; set; }
 }
--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -5,5 +5,16 @@ namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
+    public DbSet<City> Cities { get; set; } = null!;
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
     public TrybeHotelContext() { }
 }
