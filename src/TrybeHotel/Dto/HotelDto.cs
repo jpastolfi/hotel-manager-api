6191c5760c3998047af0ea656ec0062ee6b581bf
Joao Astolfi <jpastolfi@gmail.com>
2024-01-26 11:27:15 +0000

    refactor: adicionado state ao POST de Hotel

--- src/TrybeHotel/Dto/HotelDto.cs
+++ src/TrybeHotel/Dto/HotelDto.cs
@@ -7,5 +7,6 @@ namespace TrybeHotel.Dto
         public string? Address { get; set; }
         public int CityId { get; set; }
         public string? CityName { get; set; }
+        public string? State { get; set; }
     }
 }
